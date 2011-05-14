a464e5c2675c4ef8a11f8cdffbc534fa14ab0d6d
Nathan Stier <Nathan.Stier@gmail.com>
2011-05-14 16:07:25 -0500

    Added user following

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -365,6 +365,7 @@ describe UsersController do
     end
     
     describe "as a non-admin user" do
+      
       it "should protect the page" do
         test_sign_in(@user)
         delete :destroy, :id => @user
@@ -394,6 +395,43 @@ describe UsersController do
         lambda do
           delete :destroy, :id => @admin
         end.should change(User, :count).by(0)
+      end
+    end
+  end
+  
+  describe "follow pages" do
+    
+    describe "when not signed in" do
+      
+      it "should protect 'following'" do
+        get :following, :id => 1
+        response.should redirect_to(signin_path)
+      end
+      
+      it "should protect 'followers'" do
+        get :followers, :id => 1
+        response.should redirect_to(signin_path)
+      end
+    end
+    
+    describe "when signed in" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @other_user = Factory(:user, :email => Factory.next(:email))
+        @user.follow!(@other_user)
+      end
+      
+      it "should show user following" do
+        get :following, :id => @user
+        response.should have_selector("a", :href => user_path(@other_user),
+                                           :content => @other_user.name)
+      end
+      
+      it "should show user followers" do
+        get :followers, :id => @other_user
+        response.should have_selector("a", :href => user_path(@user),
+                                           :content => @user.name)
       end
     end
   end
