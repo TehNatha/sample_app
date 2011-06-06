d23d5f91da1c8d127eabbdc4a5bfa15dcbf6fe7e
Nathan Stier <Nathan.Stier@gmail.com>
2011-06-06 16:39:24 -0500

    Chapter 11 Exercise 4

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -102,12 +102,28 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
   
-    it "should show the user's microposts" do
-      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
-      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
-      get :show, :id => @user
-      response.should have_selector("span.content", :content => mp1.content)
-      response.should have_selector("span.content", :content => mp2.content)
+    describe "microposts" do
+      
+      it "should show the user's microposts" do
+        mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+        mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+        get :show, :id => @user
+        response.should have_selector("span.content", :content => mp1.content)
+        response.should have_selector("span.content", :content => mp2.content)
+      end
+      
+      it "should paginate microposts" do
+        60.times do
+          mp0 = Factory(:micropost, :user => @user, :content => "Duck")
+        end
+        get :show, :id => @user
+        response.should have_selector("div.pagination")
+        response.should have_selector("span.disabled", :content => "Previous")
+        response.should have_selector("a", :href => "/users/1?page=2",
+                                           :content => "2")
+        response.should have_selector("a", :href => "/users/1?page=2",
+                                           :content => "Next")
+      end
     end
     
     it "should have the right follower/following counts" do
